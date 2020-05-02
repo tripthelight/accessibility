75ef03684e0f84f021012750b4a82d423930ad7b
Sin Kim <bye_ok@naver.com>
2020-05-02 19:00:15 +0900

    index init

---
+++ src/components/Index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="indexWrap">
+    <p>Mobile Web</p>
+    <p>Accessibility</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/Example.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="example">
+    <h4>Accessibility Example</h4>
+    <div class="navSub">
+      <router-link to="/">aria-checked</router-link>
+    </div>
+  </div>
+</template>
