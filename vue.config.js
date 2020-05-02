c32d529f999b3499619c676d67685dca06fcb52c
Sin Kim <bye_ok@naver.com>
2020-05-02 23:10:22 +0900

    index init

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/accessibility/'
-    : '/',
-  /*chainWebpack: (config) => {
-    config
-      .plugin('html')
-      .tap(args => {
-        args[0].title = 'MyApp title';
-        args[0].meta = {viewport: 'width=device-width,initial-scale=1,user-scalable=no'};
-      return args;
-    })
-  }*/
+    : '/'
 }
